6af78dbbffdeb4c2841e3c8d13b775b4917b0367
KirillRomanenko <kirill.romanenko@am-intech.ru>
2021-11-15 15:38:57 +0300

    Created main class and frame base

--- src/main/java/door/MainClass.java
+++ src/main/java/door/MainClass.java
@@ -1,10 +1,12 @@
 package door;
 
+import gui.WorkFrame;
+
 public class MainClass {
 
 
 
     public static void main(String[] args) {
-
+        new WorkFrame();
     }
 }
---
+++ src/main/java/gui/WorkFrame.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class WorkFrame extends JFrame {
+
+
+    public WorkFrame() {
+
+    }
+}
