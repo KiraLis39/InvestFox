f24e23c6b013c67b47b97a6dd4ea2ddfc79378b4
k.romanenko <k.romanenko@digital-sector.ru>
2023-05-04 11:16:19 +0300

    Restored parser

--- src/main/java/ru/investment/config/constants/ParserMessages.java
+++ src/main/java/ru/investment/config/constants/ParserMessages.java
@@ -4,6 +4,7 @@ public final class ParserMessages {
     public static final String PAGE_NOT_ACCEPTABLE = "Страница не доступна";
     public static final String TECHNICAL_WORKS = "Технические работы";
     public static final String PAGE_NOT_EXISTS = "Страница не существует";
+    public static final String PAGE_NOT_FOUND = "404 Not Found";
 
     private ParserMessages() {
     }
